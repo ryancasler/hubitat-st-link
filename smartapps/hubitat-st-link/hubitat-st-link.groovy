77bbbb711bb116e57f3d2bd461a5720936ee34e1
ryancasler <ryancasler@users.noreply.github.com>
2020-01-04 22:32:59 -0500

    Updates

--- smartapps/hubitat-st-link/hubitat-st-link.groovy
+++ smartapps/hubitat-st-link/hubitat-st-link.groovy
@@ -7,12 +7,12 @@ definition(
     name:"Send To SmartThnigs",
     namespace: "ryancasler",
     author: "Ryan Casler",
-    description: "",
+    description: "DESCRIPTION",
     category: "Convenience",
     
-    iconUrl: "",
-    iconX2Url: "",
-    iconX3Url: "",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png"
 )
 
 preferences {
